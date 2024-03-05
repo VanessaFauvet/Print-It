19bd057a5872676829dabef03db08f9f50dba2ec
VanessaF <89605309+VanessaFauvet@users.noreply.github.com>
2024-03-05 16:58:24 +0100

    Modifications sur le slide

--- assets/script.js
+++ assets/script.js
@@ -17,33 +17,73 @@ const slides = [
 	}
 ]
 
+let currentSlideIndex = 0
+
 console.log(slides.length)
 
 // Ajout d'Event Listeners sur les flèches et sur les bullet points
 let leftBtn = document.querySelector(".arrow_left")
 leftBtn.addEventListener("click", function() {
-	console.log("Vous avez cliqué sur la flèche gauche")
+	console.log("Clic flèche gauche")
+	updateSlide(currentSlideIndex - 1)
 })
 
 let rightBtn = document.querySelector(".arrow_right")
 rightBtn.addEventListener("click", function() {
-	console.log("Vous avez cliqué sur la flèche droite")
+	console.log("Clic flèche droite")
+	updateSlide(currentSlideIndex + 1)
 })
 
 let activeDot = document.querySelector(".dots")
 activeDot.addEventListener("click", function() {
-	console.log("Vous avez cliqué sur le bullet point")
+	console.log("Clic bullet point")
 })
 
-// Ajout des bullet points sur le slider
+// Ajout des bullet points sur le slider et les rendre cliquables pour changer de slide
 const dotDiv = document.querySelector(".dots")
 
 for(let i = 0; i < slides.length; i++) {
 	const dot = document.createElement("span")
 	dot.classList.add("dot") 
 	dotDiv.appendChild(dot)
+
+	dot.addEventListener("click", function () {
+        console.log("Clic bullet point")
+        updateSlide(i)
+    });
 }
 
 // Différencier le slide en cours de visionnage en remplissant le bullet point 
-const currentSlide = dotDiv.querySelector(".dot");
-currentSlide.classList.add("dot_selected");
+const currentSlide = dotDiv.querySelector(".dot")
+currentSlide.classList.add("dot_selected")
+
+// Fonction pour mettre à jour le slider avec les données du slide spécifié
+function updateSlide(index) {
+    const bannerImg = document.querySelector(".banner-img");
+    const tagLine = document.querySelector("#banner p");
+    const dots = document.querySelectorAll(".dot");
+
+    // Assurer que l'index reste dans les limites du tableau des slides
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
+
+    // Mettre à jour le slide actuel
+    currentSlideIndex = index;
+    bannerImg.src = "./assets/images/slideshow/" + slides[index].image;
+    tagLine.innerHTML = slides[index].tagLine;
+
+    // Mettre à jour les points
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add("dot_selected");
+        } else {
+            dot.classList.remove("dot_selected");
+        }
+    });
+}
+
+// Mettre à jour le slide initial
+updateSlide(currentSlideIndex);
